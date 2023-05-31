9d4be6e1d3af1432fdfbc50d7dfb232f5b2b4bee
Kawiz <92087044+Kawiz@users.noreply.github.com>
2023-05-31 15:03:27 +0200

    Add files via upload

---
+++ Z24final/Z24-main/Z24-main/Z24/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("T16")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("T16")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("0978cfda-e306-4555-950a-b72b04d8c0c0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
